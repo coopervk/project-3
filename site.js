78588b9f1a938ca3a4551e578116857ebf8cecbf
Cooper Van Kampen <coopervk@gmail.com>
2017-12-06 14:24:15 -0600

    Finish home page jquery, remove logging

--- site.js
+++ site.js
@@ -38,24 +38,20 @@ $.noConflict();
   }
 
   if ( $('#home').length ) {
-    console.log("Congrats, you're on the home page!");
     var oldLink = "https://github.com/coopervk";
     var newLink = userToAPI(oldLink);
-    console.log(newLink);
     if (newLink) {
       $.get(newLink, function( data ) {
         var imageLink;
         var bio;
         data = JSON.stringify(data);
-        console.log(data);
         regex_getImageLink = /avatar_url\"\:\"([^]+)\"\,\"gravatar_id/;
         regex_getBio = /bio\"\:\"([^]+)\",\"public_repos/;
         imageLink = regex_getImageLink.exec(data);
         imageLink = imageLink[1];
-        console.log(imageLink);
         bio = regex_getBio.exec(data);
         bio = bio[1];
-        console.log(bio);
+        $("aside").prepend( '<figure><img src=' + imageLink + ' alt="github profile picture" /><figcaption>(github bio) ' + bio + '</figure>' );
       });
     }
   }
@@ -78,9 +74,7 @@ $.noConflict();
           if (updateTag) {
             $(updateTag).replaceWith( '<li class="update"><strong>Latest Update:</strong> ' + commitMessage + '</li>' );
           }
-          console.log("\n\n");
         });
-      console.log("\n\n");
       }
     });
   }
